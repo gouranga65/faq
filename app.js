bd33d8f6b8fe73a8505490dd696a541e117ee400
sundar <gm49302@gmail.com>
2022-11-28 22:28:22 +0530

    almost done

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const remove=document.querySelector(".remove").addEventListener("click",function
 })
 const answerTwo=document.querySelector(".answerTwo");
 const addTwo=document.querySelector(".addTwo").addEventListener("click",function(){
-    answerTwo.classList.add("deactive");
+    answerTwo.classList.add("active");
 })
 const removeTwo=document.querySelector(".removeTwo").addEventListener("click",function(){
-    answerTwo.classList.remove("deactive");
+    answerTwo.classList.remove("active");
+})
+const answerThree=document.querySelector(".answerThree");
+const addThree=document.querySelector(".addThree").addEventListener("click",function(){
+    answerThree.classList.add("active");
+})
+const removeThree=document.querySelector(".removeThree").addEventListener("click",function(){
+    answerThree.classList.remove("active");
 })
